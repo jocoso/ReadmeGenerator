ceca3a328acae72d72fb7f4a1e21dbcb0507d001
jocoso <joshua.collado022@gmail.com>
2024-06-13 19:54:51 -0400

    Added the run method to READMEGenerator

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ class READMEGenerator {
         this.content = `# ${this.name}\n`;
         return this.content;
     }
+
+    run() {
+        // Ask the user for information
+        // call the corresponding function to generate the content
+        // When all the information is collected, call the create function to generate the README.md file
+    }
 }
 
 export default READMEGenerator;
