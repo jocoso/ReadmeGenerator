c65d7e12c49faaa462df9391dbb80c514f12e9b5
jocoso <joshua.collado022@gmail.com>
2024-06-13 21:46:54 -0400

    Implemented various changes: 1. eliminated unnecesary babel file 2. changed repository structure

---
+++ module/readme-generator.js
@@ -0,0 +1,21 @@
+const inquirer = require('inquirer');
+
+class READMEGenerator {
+    constructor() {
+        this.name = 'README.md';
+        this.content = '';
+    }
+
+    create() {
+        this.content = `# ${this.name}\n`;
+        return this.content;
+    }
+
+    run() {
+        // Ask the user for information
+        // call the corresponding function to generate the content
+        // When all the information is collected, call the create function to generate the README.md file
+    }
+}
+
+export default READMEGenerator;
