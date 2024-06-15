ec312105ac031f1974e09a0ce259d1a1ac145555
jocoso <joshua.collado022@gmail.com>
2024-06-15 18:00:45 -0400

    Reverting pull request

--- src/readme-generator.js
+++ src/readme-generator.js
@@ -8,39 +8,12 @@ class READMEGenerator {
         this.content = '';
     }
 
-    async generateTitle() {
-        // Ask user for title
-        // Add h1 title to content
-        const answer = await inquirer.prompt([
-            {
-                name: 'title',
-                message: "What is the title of your project?"
-            }
-        ]);
-
-        this.content = `# ${answer.title}\n`;
-    }
-
-    printContent() {
-        console.log(this.content);
-        return this.content;
-    }
-
     create() {
-        fs.writeFile(this.name, this.content, (err) => {
-            if(err) {
-                return console.log(err);
-            }
 
-            console.log('The file has been saved!');
-        });
     }
 
     async run() {
         // Ask the user for information
-        await this.generateTitle();
-        this.printContent();
-        this.create();
         // call the corresponding function to generate the content
         // When all the information is collected, call the create function to generate the README.md file
     }
