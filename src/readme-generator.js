dab9554d0087c3ee8a966d3870492866e110d9cf
jocoso <joshua.collado022@gmail.com>
2024-06-16 11:20:11 -0400

    feat: Add Table of Contents generation to READMEGenerator
    - Implemented generateTOC method to create a structured Table of Contents in the README file.
    - Updated run method to include the generateTOC method.
    - Added tests to verify the correct generation of Table of Contents.
    - Ensured fs.writeFile is called with appropriate arguments during creation.
    - Maintained and expanded Jest test coverage to validate the new functionality.

--- src/readme-generator.js
+++ src/readme-generator.js
@@ -8,6 +8,16 @@ class READMEGenerator {
         this.content = '';
     }
 
+    generateTOC() {
+        this.content += '## Table of Contents\n';
+        this.content += '- [Description](#description)\n';
+        this.content += '- [Installation](#installation)\n';
+        this.content += '- [Usage](#usage)\n';
+        this.content += '- [License](#license)\n';
+        this.content += '- [Contributors](#contributors)\n';
+        this.content += '- [Tests](#tests)\n';
+    }
+
     create() {
         fs.writeFile(this.name, this.content, (err) => {
             if (err) {
@@ -21,7 +31,8 @@ class READMEGenerator {
         // Ask the user for information
         // call the corresponding function to generate the content
         // When all the information is collected, call the create function to generate the README.md file
-
+        this.generateTOC();
+        this.create();
     }
 }
 
